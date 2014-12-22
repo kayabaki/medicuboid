acaeaa47491546e986a452bf6789e91b0724d658
Kayaba Akihito <kayabako@gmail.com>
2014-12-22 14:54:42 -0400

    main medicuboid program

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <kayaba.h>
+
+int main(){
+	int medi = 4532;
+	int cuboid = 0;
+
+	int medicuboid = medi / cuboid; // this division by zero is what makes all the magic happen ;)
+
+	return medicuboid;
+}
